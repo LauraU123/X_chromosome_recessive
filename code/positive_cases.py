04419029cd4754052e8289795cf944d95dc4a3b7
Laura Urbanska <laura.urbanska@unibe.ch>
2024-10-31 20:54:13 +0100

    script to extract positive cases

--- code/positive_cases.py
+++ code/positive_cases.py
@@ -5,8 +5,14 @@ import argparse
 
 def extract_positive_cases(filepath):
     """This script extracts only the positive cases - i.e. the offspring """
-    df = pd.read_csv(filepath, sep=" ")
+    df = pd.read_csv(filepath, sep=" ", header=None)
     df.columns = ["FID", "ID", "Father", "Mother", "Sex", "Phenotype"]
-    print(df)
+    positive = df.loc[df['Phenotype'] == 2, 'ID']
+    output = []
+    # convert to list
+    for i in positive: output.append(i)
+    return output
+
+# get the sequences from the input files and then convert them to the correct format. Then as before.
 
 extract_positive_cases("Xchr/data/FAM1_2024Oct14.fam")
